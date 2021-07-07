baf27a2f34cd28942ea52ed493f7e723914b7bee
Connor Ashley <ashleycm@vcu.edu>
2021-07-07 18:34:40 -0400

    sort state.jokes array every render based on votes, display jokes in that order

--- src/JokeList.js
+++ src/JokeList.js
@@ -72,6 +72,7 @@ class JokeList extends Component {
                 </div>
             )
         }
+        let jokes = this.state.jokes.sort((a,b) => b.votes - a.votes);
         return (
             <div className="JokeList">
                 <div className="JokeList-sidebar">
@@ -84,7 +85,7 @@ class JokeList extends Component {
                     <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
                 </div>
                 <div className="JokeList-jokes">
-                    {this.state.jokes.map(j => (
+                    {jokes.map(j => (
                         <Joke 
                             text={j.text}
                             votes={j.votes}
