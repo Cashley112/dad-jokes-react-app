92448f6b888be7d697d68e24a6e28114ae6e08f7
Connor Ashley <ashleycm@vcu.edu>
2021-07-07 17:50:56 -0400

    refactoring methods that manipualte state to also update localStorage so that jokes and their corresponding votes can be persisted through rerenders and fetching of new jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,6 +11,7 @@ class JokeList extends Component {
     constructor(props) {
         super(props);
         this.state = { jokes: JSON.parse(window.localStorage.getItem("jokes") || '[]') };
+        this.handleClick = this.handleClick.bind(this);
     }
     componentDidMount(){
         // Load Jokes
@@ -26,11 +27,11 @@ class JokeList extends Component {
             );
             jokes.push({ text: res.data.joke, votes: 0, id: uuidv4() })
         }
-        this.setState({ jokes: jokes })
-        window.localStorage.setItem(
-            'jokes',
-            JSON.stringify(jokes)
-        )
+        this.setState(st => ({
+            jokes: [...st.jokes, ...jokes]
+        }),
+        () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+        );
     }
     handleVote(id, delta) {
         this.setState(
@@ -38,7 +39,12 @@ class JokeList extends Component {
                 jokes: st.jokes.map(j => 
                     j.id === id ? { ...j, votes: j.votes + delta } : j
                 )
-            }));
+            }), 
+            () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+            );
+    }
+    handleClick() {
+        this.getJokes();
     }
     render () {
         return (
@@ -50,7 +56,7 @@ class JokeList extends Component {
                     <img 
                         alt="smd" src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' 
                     />
-                    <button className="JokeList-getmore">New Jokes</button>
+                    <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
                 </div>
                 <div className="JokeList-jokes">
                     {this.state.jokes.map(j => (
