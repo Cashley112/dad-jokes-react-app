16f87ab266a84e6bcab40db97cf1528ae97c4bd7
Connor Ashley <ashleycm@vcu.edu>
2021-07-08 09:46:39 -0400

    styled fetch button, added shaking animation to emoji in JokeList-sidebar

--- src/JokeList.js
+++ src/JokeList.js
@@ -19,9 +19,7 @@ class JokeList extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
     componentDidMount(){
-        // Load Jokes
-        if (this.state.jokes.length === 0) this.getJokes();
-        
+        if (this.state.jokes.length === 0) this.getJokes(); 
     }
     async getJokes(){
         try {
@@ -82,7 +80,7 @@ class JokeList extends Component {
                     <img 
                         alt="smd" src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' 
                     />
-                    <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
+                    <button className="JokeList-getmore" onClick={this.handleClick}>Fetch Jokes</button>
                 </div>
                 <div className="JokeList-jokes">
                     {jokes.map(j => (
